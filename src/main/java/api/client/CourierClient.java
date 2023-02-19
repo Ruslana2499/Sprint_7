181c847201d00733ff01d3198de0af6218bc8f79
RuslanaManbatchurina <ruslanany66@gmail.com>
2023-02-19 17:38:45 +0500

    review changes

--- src/main/java/api/client/CourierClient.java
+++ src/main/java/api/client/CourierClient.java
@@ -4,38 +4,26 @@ import common.constants.Constants;
 import common.entities.Courier;
 import common.entities.CourierAuth;
 import common.entities.IdResult;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
-
-public class CourierClient {
+public class CourierClient extends BaseApiClient {
+    @Step("Авторизация курьера")
     public Response authCourier(CourierAuth body) {
-        return given()
-                .header("Content-type", "application/json")
-                .auth().oauth2(Constants.BEARER_TOKEN)
-                .and()
-                .body(body)
-                .when()
-                .post("/api/v1/courier/login");
+        return this.post(Constants.LOGIN_URI, body);
     }
 
+    @Step("Создание курьера")
     public Response createCourier(Object body) {
-        return given()
-                .header("Content-type", "application/json")
-                .auth().oauth2(Constants.BEARER_TOKEN)
-                .and()
-                .body(body)
-                .when()
-                .post("/api/v1/courier");
+        return this.post(Constants.COURIER_URI, body);
     }
 
+    @Step("Удаление курьера")
     public void deleteCourier(Courier courier) {
         CourierAuth courierAuth = new CourierAuth(courier);
         Response response = authCourier(courierAuth);
         IdResult idResult = response.body().as(IdResult.class);
-        given()
-                .auth().oauth2(Constants.BEARER_TOKEN)
-                .delete("/api/v1/courier/" + idResult.getId());
+        this.delete(Constants.COURIER_URI + "/" + idResult.getId());
     }
 
 }
--- src/main/java/api/client/OrderClient.java
+++ src/main/java/api/client/OrderClient.java
@@ -2,18 +2,12 @@ package api.client;
 
 import common.constants.Constants;
 import common.entities.Order;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
-import static io.restassured.RestAssured.given;
-
-public class OrderClient {
-    public Response createOrder(Order order){
-        return given()
-                .header("Content-type", "application/json")
-                .auth().oauth2(Constants.BEARER_TOKEN)
-                .and()
-                .body(order)
-                .when()
-                .post("/api/v1/orders");
+public class OrderClient extends BaseApiClient {
+    @Step("Создание заказа")
+    public Response createOrder(Order order) {
+        return this.post(Constants.ORDERS_URI, order);
     }
 }
